b78b92560836ed82013733a2773fa1eefa6d8af9
Declan <declanmcgrellis3@gmail.com>
2023-07-16 21:15:07 -0400

    added test render for snb sprite sheet

--- src/game.cpp
+++ src/game.cpp
@@ -3,5 +3,18 @@
 
 void game(SDL_Renderer* renderer, inputHandler* handler) {
 
-    
+    animatedObj snb("assets/SNB_SpriteSheet.png", renderer, 9, 58, 58, 500);
+
+    while(!handler->quit) {
+
+        handler->handle();
+
+        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+        snb.animRender(renderer, SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 200, 200);
+
+        SDL_RenderPresent(renderer);
+    }
+
 }
